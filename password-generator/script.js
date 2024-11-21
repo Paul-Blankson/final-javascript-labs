5a4876d7bda39c74532f98d03ad11474503ae1fc
Paul Blankson <paul.blankson@amalitech.com>
2024-11-21 14:07:24 +0000

    refactor: defined a PasswordGenerator class and set the properties to appropriate dom element

---
+++ password-generator/script.js
@@ -0,0 +1,21 @@
+class PasswordGenerator {
+    constructor() {
+        this.passwordDisplay = document.querySelector('.password__input');
+        this.copyButton = document.querySelector('.password__copy');
+        this.generateButton = document.querySelector('.password__generate');
+        this.lengthSlider = document.querySelector('.length__slider');
+        this.lengthValue = document.querySelector('.length__value');
+        this.checkboxes = document.querySelectorAll('.options__checkbox');
+        this.strengthText = document.querySelector('.strength__text');
+        this.strengthBars = document.querySelectorAll('.strength__bar');
+
+        this.characterSets = {
+            lowercase: 'abcdefghijklmnopqrstuvwxyz',
+            uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            numbers: '0123456789',
+            symbols: '!@#$%^&*()_+-=[]{}|;:,.<>?'
+        };
+
+    }
+
+}
